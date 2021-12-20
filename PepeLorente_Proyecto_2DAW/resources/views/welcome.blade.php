f32f73d4a8b388d5c87cd857b6c233d22dfa1c0c
Jose Sanchez PC <sanchez069@gmail.com>
2021-12-20 12:58:32 +0100

    añadido vista Login

--- PepeLorente_Proyecto_2DAW/resources/views/welcome.blade.php
+++ PepeLorente_Proyecto_2DAW/resources/views/welcome.blade.php
@@ -20,8 +20,7 @@
             }
         </style>
         <!-- BOOSTRAP CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    </head>
+        <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>     </head>
     <body class="antialiased">
         <h4 class="container alert alert-success" style="margin:35px auto;padding:10px">BOOSTRAP 5 WORKING!!!</h4>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -132,6 +131,6 @@
             </div>
         </div>
         <!-- BOOSTRAP JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>
